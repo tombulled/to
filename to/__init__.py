9a9508cb1720eae228a792178837d449adb21d4e
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-06-26 11:29:24 +0100

    Enable searching on url domain

--- to/__init__.py
+++ to/__init__.py
@@ -4,10 +4,11 @@ import thefuzz.fuzz
 import yaml
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+import furl
 
 from .models import Bookmark
 
-THRESHOLD: int = 15
+THRESHOLD: int = 33
 
 app: FastAPI = FastAPI()
 
@@ -24,6 +25,16 @@ def get_bookmarks() -> List[Bookmark]:
     return [Bookmark(**bookmark) for bookmark in load_db()]
 
 
+def get_ratio(bookmark: Bookmark, query: str) -> int:
+    return max(
+        thefuzz.fuzz.ratio(source, query)
+        for source in (
+            bookmark.title,
+            furl.furl(bookmark.url).host.replace(".", " ")
+        )
+    )
+
+
 @app.get("/")
 def get_root() -> List[dict]:
     return load_db()
@@ -34,7 +45,7 @@ def get_search(q: str) -> List[dict]:
     ratios: Dict[Bookmark, int] = {
         bookmark: ratio
         for bookmark in get_bookmarks()
-        if (ratio := thefuzz.fuzz.ratio(bookmark.title, q)) > THRESHOLD
+        if (ratio := get_ratio(bookmark, q)) > THRESHOLD
     }
 
     return sorted(ratios.keys(), key=ratios.get, reverse=True)
